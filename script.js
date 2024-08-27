6b9d44e05b214446733fd1855d6089c85b7f28ce
R.Alizadeh <R.Alizadeh@emofid.com>
2024-08-27 11:38:16 +0330

    feat: add date convertor

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+$(function () {
+    const today = new Date();
+    const gDate=today.toISOString().substring(0, 10);
+    $("#txtGDate").val(gDate);
+    $("#txtJDate").val(gToJ(gDate));
+    $("#txtGDate").change(() => {
+        $("#txtGDate").removeClass("border-warning");
+    });
+    $("#txtJDate").change(() => {
+        $("#txtJDate").removeClass("border-warning");
+    });
+    $("#btnGToJ").click(() => {
+        try {
+            const gDate = $("#txtGDate").val();
+            const jDate=gToJ(gDate);
+            $("#txtJDate").val(jDate);
+        }
+        catch {
+            $("#txtGDate").addClass("border-warning");
+        }
+    });
+    $("#btnJToG").click(() => {
+        try {
+            const date =jToG($("#txtJDate").val());
+            $("#txtGDate").val(date);
+        }
+        catch {
+            $("#txtJDate").addClass("border-warning");
+        }
+    });
+});
+
+function jToG(jDate){
+    const jDateArr = jDate.split(/[^0-9]/);
+    const gDate = jalaali.jalaaliToDateObject(parseInt(jDateArr[0]), parseInt(jDateArr[1]), parseInt(jDateArr[2]));
+    return gDate.toISOString().substring(0, 10);
+}
+function gToJ(gDate){
+    const date = new Date(gDate);
+     return new Intl.DateTimeFormat('fa-IR-u-nu-latn').format(date);
+}
