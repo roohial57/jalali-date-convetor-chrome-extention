0d5ab6f483c0e3ca41c496e1abc64779bf906df5
R.Alizadeh <R.Alizadeh@emofid.com>
2024-08-27 12:49:33 +0330

    fix: add formatDate

--- script.js
+++ script.js
@@ -33,16 +33,22 @@ $(function () {
 });
 function goToToday() {
     const today = new Date();
-    const gDate = today.toISOString().substring(0, 10);
+    const gDate = formatDate(today);
     $("#txtGDate").val(gDate);
     $("#txtJDate").val(gToJ(gDate));
 }
 function jToG(jDate) {
     const jDateArr = jDate.split(/[^0-9]/);
     const gDate = jalaali.jalaaliToDateObject(parseInt(jDateArr[0]), parseInt(jDateArr[1]), parseInt(jDateArr[2]));
-    return gDate.toISOString().substring(0, 10);
+    return formatDate(gDate);
 }
 function gToJ(gDate) {
     const date = new Date(gDate);
     return new Intl.DateTimeFormat('fa-IR-u-nu-latn').format(date);
+}
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}/${month}/${day}`;
 }
